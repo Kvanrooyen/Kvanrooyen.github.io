70a4b4ce21aac251cfbb66b3caa527944f12aab6
Keagan <governor.keagan@outlook.com>
2020-04-07 18:05:08 +0100

    Chore: Updated dependencies
    Updated the dependiecs to fix some security issues and removed unused dependincies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,48 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const autoprefixer = require("gulp-autoprefixer");
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
-const browserSync = require("browser-sync").create();
-const AUTOPREFIXER_BROWSERS = [
-  "ie >= 10",
-  "ie_mob >= 10",
-  "ff >= 30",
-  "chrome >= 34",
-  "safari >= 7",
-  "opera >= 23",
-  "ios >= 7",
-  "android >= 4.4",
-  "bb >= 10"
-];
+const browserSync = require("browser-sync");
 
-//Compile SCSS to CSS
+// Compile SCSS to CSS
 function style() {
-  // 1. Find SCSS file
+  // Find SCSS file
   return (
     gulp
-      .src("src/scss/**/*.scss")
-      // 2. Pass file through SASS compiler
+      .src("src/scss/*.scss")
+      // Pass file through SASS compiler
       .pipe(
         sass({
           outputStyle: "nested",
           precision: 10,
           includePaths: ["."],
-          onError: console.error.bind(console, "Sass error:")
+          onError: console.error.bind(console, "SASS Error: "),
         })
       )
-      .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
-      // 3. Where to save CSS file
-      .pipe(gulp.dest("css"))
-      // 3.5 Minify CSS
+      // Where to save CSS file
+      .pipe(gulp.dest("src/css"))
+      // Minify CSS
       .pipe(csso())
-      //Rename minified CSS file
+      // Rename minified CSS file
       .pipe(rename("main.min.css"))
-      //Save minified CSS file
-      .pipe(gulp.dest("css"))
-      // 4. Stream changes to all browsers
+      // Save minified CSS file
+      .pipe(gulp.dest("src/css"))
+      // Stream changes to all browsers
       .pipe(browserSync.stream())
   );
 }
 
-//Watch and update
+// Watch and update
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "."
-    }
+      baseDir: ".",
+    },
   });
-  gulp.watch("src/scss/**/*.scss", style);
+  gulp.watch("src/scss/*.scss", style);
   gulp.watch("*.html").on("change", browserSync.reload);
-  gulp.watch("src/scss/**/*.scss").on("change", browserSync.reload);
+  gulp.watch("src/scss/*.scss").on("change", browserSync.reload);
+  gulp.watch("src/js/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
